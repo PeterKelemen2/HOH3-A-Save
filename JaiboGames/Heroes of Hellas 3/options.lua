f7164ac8774479dc5daf1d5950ee7e4dacd3fa50
PeterKelemen2 <90985214+PeterKelemen2@users.noreply.github.com>
2023-03-31 11:19:36 +0200

    Add files via upload

---
+++ JaiboGames/Heroes of Hellas 3/options.lua
@@ -0,0 +1,4 @@
+mouse_sen = 6;
+sound_volume = 22;
+music_volume = 22;
+is_cursor_system = true;
---
+++ JaiboGames/Heroes of Hellas 3/profile_list.lua
@@ -0,0 +1,5 @@
+next_uin = 3;
+active_num = 0;
+is_live = { 1,0,0,0,0,0 };
+name = { "Peti","","","","","" };
+slot = { 0,1,2,3,4,5 };
